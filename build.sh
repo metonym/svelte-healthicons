d0b16d65ad7018fc6000a51ddac3e8f9d38c9796
Eric Liu <ericyl.us@gmail.com>
2021-12-17 10:35:30 -0800

    chore: print comments in shell script

--- build.sh
+++ build.sh
@@ -1,2 +1,7 @@
+#!/bin/bash
+
+echo "removing 'node_modules/healthicons'"
 rm -rf node_modules/healthicons
+
+echo "downloading 'resolvetosavelives/healthicons/public/icons/svg'..."
 npx degit resolvetosavelives/healthicons/public/icons/svg node_modules/healthicons
